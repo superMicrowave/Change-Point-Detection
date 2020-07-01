1b793e3c03f18381a9b791ffb8e1e571bdbce197
ee <ws342@nau.edu>
2020-06-30 17:42:39 -0700

    1

--- accuracy.py
+++ accuracy.py
@@ -1,9 +1,12 @@
 
 from L1_predection import accuracy_list
+from baseline import test_accuracy
 import matplotlib.pyplot as plt
 
 test_fold_num = 6
+
 plt.scatter(accuracy_list, test_fold_num * ['L1_pre'], color='black')
+plt.scatter(test_accuracy, test_fold_num * ['Baseline'], color='green')
 plt.xlabel("accuracy.percent %")
 plt.ylabel("algorithm")
 plt.tight_layout()
