05a04799f1dce1b7623e8ff4566e74b0de7eb3b3
ee <ws342@nau.edu>
2020-07-02 17:13:14 -0700

    1

--- accuracy.py
+++ accuracy.py
@@ -1,13 +1,13 @@
 
 from L1_predection import accuracy_list
-from baseline import base_test_acc
+from baseline import line_test_acc
 from SpatialPyramidPooling import cnn_test_acc
 import matplotlib.pyplot as plt
 
 test_fold_num = 6
 
 plt.scatter(accuracy_list, test_fold_num * ['L1_pre'], color='black')
-plt.scatter(base_test_acc, test_fold_num * ['Baseline'], color='green')
+plt.scatter(line_test_acc, test_fold_num * ['Line'], color='green')
 plt.scatter(cnn_test_acc, test_fold_num * ['Cnn'], color='blue')
 plt.xlabel("accuracy.percent %")
 plt.ylabel("algorithm")
